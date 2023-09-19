d28d05795eb5099fa9db0fe2e5a474f4bd41cd08
ManhAnNguyen <thungantn1999@gmail.com>
2023-09-19 08:24:49 +0700

    add day 5_lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
 import React from "react";
-import Count from "./component/Count";
-import Person from "./component/Person";
-import Husband from "./component/Husband";
 
 const App = () => {
+  const onClick = () => {
+    const value = prompt("Enter name");
+
+    console.log(value);
+  };
   return (
     <div>
-      <Count />
-      <Husband />
+      'A,B'
+      <button onClick={onClick}>Add</button>
+      <button onClick={onClick}>rest</button>
     </div>
   );
 };
 
 export default App;
-
-// import React, { useState } from "react";
-
-// const App = () => {
-//   const [countState, setCountState] = useState(0);
-//   let countNormal = 0;
-
-//   const onClickState = () => {
-//     setCountState(100000);
-//   };
-
-//   const onClickNormal = () => {
-//     countNormal = 100000;
-//   };
-
-//   console.log("re-render");
-//   return (
-//     <div>
-//       <h1>countState : {countState}</h1>
-//       <h1>countNormal : {countNormal}</h1>
-//       <button onClick={onClickState}>onClickState!</button>
-//       <button onClick={onClickNormal}>onClickNormal!</button>
-//     </div>
-//   );
-// };
-
-// export default App;
