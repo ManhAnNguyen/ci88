90f7ba8f248f3e885ef6b1867c7beef0d39b01a0
ManhAnNguyen <thungantn1999@gmail.com>
2023-09-29 22:22:12 +0700

    add day8_lab

--- src/TrafficLight.jsx
+++ src/TrafficLight.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { NumericFormat } from "react-number-format";
 import { Button, Input } from "antd";
+
+const arr = ["red", "yellow", "green"];
+
 const TrafficLight = () => {
+  const [start, setStart] = useState(false);
+  const [value, setValue] = useState("");
+  const [index, setIndex] = useState(null);
+
+  useEffect(() => {
+    if (!start || !value) return;
+    const timer = setInterval(() => {
+      const currentValue = value - 1;
+      setValue(currentValue);
+
+      if (currentValue > 10) {
+        setIndex(0);
+      } else if (currentValue <= 10 && currentValue > 0) {
+        setIndex(1);
+      } else {
+        setIndex(2);
+      }
+    }, 1000);
+
+    //clean up function
+    return () => clearInterval(timer);
+  }, [start, value]);
+
   return (
     <div className="traffic-light">
       <div className="count">
         {" "}
         <NumericFormat
+          value={value}
           customInput={Input}
+          onChange={(e) => setValue(e.target.value)}
           style={{
             width: "300px",
           }}
@@ -16,6 +44,7 @@ const TrafficLight = () => {
         <Button
           type="primary"
           size="middle"
+          onClick={() => setStart(true)}
           style={{
             marginLeft: "10px",
           }}
@@ -24,9 +53,15 @@ const TrafficLight = () => {
         </Button>
       </div>
       <div className="lights">
-        <div className="light red"></div>
-        <div className="light yellow"></div>
-        <div className="light green"></div>
+        {arr.map((light, i) => (
+          <div
+            className={`light `}
+            key={light}
+            style={{
+              background: i === index ? light : "",
+            }}
+          ></div>
+        ))}
       </div>
     </div>
   );
